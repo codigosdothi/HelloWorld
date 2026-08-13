820675fa451e74cb05c5e3b0444f527e8fa00662
codigosdothi <iamthisoares@outlook.com>
2026-08-13 16:48:46 -0300

    Adiciona operações de duplicação e divisão à variável

--- incrementareDecremantar.py
+++ incrementareDecremantar.py
@@ -3,6 +3,8 @@
 ## Em seguida, utilize os operadores de atribuição aritmetica para:
 # Incrementar o valor da variável em 1;
 # Decrementar o valor da variável em 1.
+# Duplique o Conteúdo da variável.
+# Dívida pela metade.
 
 dado = 16
 print(f"O valor inicial da variável é: {dado}")
@@ -12,3 +14,9 @@ print(f"Após incrementar em 1, o valor da variável é: {dado}")
 
 dado -= 1
 print(f"Após decrementar em 1, o valor da variável é: {dado}")
+
+dado *= 2
+print(f"Após duplicar o valor da variável, o valor é: {dado}")
+
+dado /= 2
+print(f"Após dividir o valor da variável pela metade, o valor é: {dado}")
