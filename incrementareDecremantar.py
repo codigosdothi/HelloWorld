2c1dd6b4db9178a91da4fe8f0b545e33fe49325e
codigosdothi <iamthisoares@outlook.com>
2026-08-13 16:43:00 -0300

    Atribuidores Aritmeticos

---
+++ incrementareDecremantar.py
@@ -0,0 +1,14 @@
+## Crie uma variável inteira e atribua um valor arbritário a ela, de sua preferência.
+## Imprima seu conteúdo 
+## Em seguida, utilize os operadores de atribuição aritmetica para:
+# Incrementar o valor da variável em 1;
+# Decrementar o valor da variável em 1.
+
+dado = 16
+print(f"O valor inicial da variável é: {dado}")
+
+dado += 1
+print(f"Após incrementar em 1, o valor da variável é: {dado}")
+
+dado -= 1
+print(f"Após decrementar em 1, o valor da variável é: {dado}")
