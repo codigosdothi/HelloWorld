f3d3bb94c49e245b6046e80cfb572a017bc6e1c3
codigosdothi <iamthisoares@outlook.com>
2026-08-13 15:31:46 -0300

    input exercicios

---
+++ Exercicio3input.py
@@ -0,0 +1,10 @@
+# Crie um programa que peça que o usuário insira seu nome, seu telefone e seu número de identidade
+# Ao final imprima as informações na tela, informadas pelo usuário.
+
+nome = input("Digite seu nome: ")
+telefone = input("Digite seu telefone: ")
+identidade = input("Digite seu número de identidade: ")
+
+print("Nome:", nome)
+print("Telefone:", telefone)
+print("Identidade:", identidade)
---
+++ exercicio2.py
@@ -0,0 +1,7 @@
+# Crie um programa que, no primeiro momento, peça para ler a idade de uma pessoa.
+# Tente imprimir a idade lida, sem utilizar variáveis.
+# Agora Introduza uma variável para armazenar o dado então imprima na tela. 
+
+anos = input("Digite sua idade: ")
+
+print("A informação digitada foi:", anos)
