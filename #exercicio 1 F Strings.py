b43338bb20c427dac9ad0716909eb6b19458b1f4
codigosdothi <iamthisoares@outlook.com>
2026-08-13 14:59:11 -0300

    Commit inicial: Primeiros passos e exercícios de Python".

---
+++ #exercicio 1 F Strings.py
@@ -0,0 +1,7 @@
+#exercicio 1 F Strings 
+
+print(f"O valor inteiro em decimal é: {10:d}")
+print(f"O valor inteiro em binário é: {10:b}")
+
+print(f"O valor de Pi é: {3.14159265:f}")
+print(f"O valor de Pi é: {3.14159:.2f}")
---
+++ helloworld.py
@@ -0,0 +1,2 @@
+print("Hello, World")
+print("Eu sou Thiago Soares. Estou aprendendo a programar, e já consigo rodar meus primeiros programas em Python.")
