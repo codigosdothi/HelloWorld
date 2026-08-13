d3eca40e300f8d5cdab95feadff442bc161a8169
codigosdothi <iamthisoares@outlook.com>
2026-08-13 20:51:35 -0300

    Keywords

---
+++ keywords.py
@@ -0,0 +1,7 @@
+
+''' < Crie um programa que atribua o valor "True" a uma variável do tipo booleano.
+Imprima este valor na tela. 
+Faça um comentário ao longo do código para sinalizar que se trata do uso de uma variável do tipo bool.> '''
+
+captain = True  # A variável 'captain' é do tipo booleano e armazena o valor True
+print(captain)  # Imprime o valor da variável 'captain' na tela.
