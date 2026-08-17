b9e5c67613080b0f2501bc1d943e62c40bc8c615
codigosdothi <iamthisoares@outlook.com>
2026-08-17 14:24:44 -0300

    Função While

---
+++ loop.py
@@ -0,0 +1,5 @@
+'''<Crie um programa que imprima a contagem de 1 a 10 na tela , usando While, para automação fazer sentido precisa estar dentro de um laço.>'''
+j = 1
+while j <= 10:
+    print(j," ", end="")
+    j+= 1
