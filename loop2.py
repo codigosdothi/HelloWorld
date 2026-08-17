8da3b91b8bacde20d7af9ce541ff48efbd67a9eb
codigosdothi <iamthisoares@outlook.com>
2026-08-17 16:19:38 -0300

    De 10 a 0 com for e range

---
+++ loop2.py
@@ -0,0 +1,6 @@
+'''< Crie um programa que imprima na tela os números de 10 a 1.
+Utilize uma estrtura de repetição do tipo "for", em um conjunto com a função "range" ()>'''
+
+for i in range(10, 0, -1):
+    print(i, "", end="")
+    
