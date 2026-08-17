4a1b604cd1e1ef53768b55826f17610397e1109a
codigosdothi <iamthisoares@outlook.com>
2026-08-17 16:10:19 -0300

    de 1 a 10 usando for e range

---
+++ estruturaForRange.py
@@ -0,0 +1,7 @@
+'''< Crie um programa que imprima na tela os números de 1 a 10.
+Utilize uma estrtura de repetição do tipo "for", em um conjunto com a função "range" ()
+A invocação da operação de saída não pode imprimir os valores literalmente.
+é preciso que o "print" imprima" o conteúdo de alguma variável, dentro do laço, para que a automação faça sentido.>'''
+
+for i in range(1, 11, 1):
+    print(i, "", end="")
