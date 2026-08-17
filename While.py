344ff2b1a908d66574cf4a207db5b8a2046185c7
codigosdothi <iamthisoares@outlook.com>
2026-08-17 14:34:33 -0300

    Encerra While com input correto

---
+++ While.py
@@ -0,0 +1,7 @@
+'''< Crie um programa que continue pedindo, repetidas vezes, para que o usuário insira um número qualquer.
+Encerra apenas quando o usuário digitar 0.>'''
+
+j = int(input("Digite um número qualquer: "))
+while j != 0:
+    j = int(input("Digite um número qualquer: "))
+    
