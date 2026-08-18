76bebf587836ff0a205acdce8cfebd583ae9ff9f
codigosdothi <iamthisoares@outlook.com>
2026-08-18 14:59:18 -0300

    Usando o Break

--- break.py
+++ break.py
@@ -1,2 +1,7 @@
 '''< Crie um programa com um laço que intere por 10 vezes, imprimindo valores na tela.
 Dentro do laço, adicione uma condição para que, caso estejamos na quinta interação, o comando break force o laço a interromper a sua excução por completo.>'''
+
+for i in range(10):
+    if i == 5:
+        break
+    print(i, "", end="")
